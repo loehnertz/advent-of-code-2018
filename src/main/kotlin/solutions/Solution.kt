f839b732817f50e649fe6d9c0af7449ab309cf92
loehnertz <jakob2102@gmail.com>
2018-12-01 01:41:55 +0100

    Create a method that lets each implemented day read in its input

--- src/main/kotlin/solutions/Solution.kt
+++ src/main/kotlin/solutions/Solution.kt
@@ -1,9 +1,19 @@
 package solutions
 
+import java.io.File
+import java.io.InputStream
+import java.nio.file.Paths
+
 abstract class Solution {
     private val relativeInputDirectoryPath = "src/main/resources/inputs"
 
     abstract val identifier: String
 
     abstract fun solve(): String
+
+    fun retrieveInput(): String {
+        val inputDirectoryPath = Paths.get("").resolve(relativeInputDirectoryPath).toAbsolutePath().toString()
+        val inputStream: InputStream = File("$inputDirectoryPath/$identifier.txt").inputStream()
+        return inputStream.bufferedReader().use { it.readText() }
+    }
 }
