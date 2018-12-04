9f372fd57677c196638c1a39a07b9dc8a24c522a
loehnertz <jakob2102@gmail.com>
2018-12-04 20:48:41 +0100

    Start development of day 4

---
+++ src/main/kotlin/solutions/Day04.kt
@@ -0,0 +1,17 @@
+package solutions
+
+object Day04 : Solution() {
+    override val identifier = this::class.simpleName.toString()
+
+    override fun solvePart1(input: String): String {
+        TODO("Not implemented yet.")
+    }
+
+    override fun solvePart2(input: String): String {
+        TODO("Not implemented yet.")
+    }
+}
+
+fun main(args: Array<String>) {
+    Day04.solve()
+}
