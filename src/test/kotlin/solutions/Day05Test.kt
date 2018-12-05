132882d713e364ed76378a1dea9c4d6a93918e16
loehnertz <jakob2102@gmail.com>
2018-12-05 23:11:14 +0100

    Add tests for the implementation of day 5

--- src/test/kotlin/solutions/Day05Test.kt
+++ src/test/kotlin/solutions/Day05Test.kt
@@ -6,11 +6,41 @@ import kotlin.test.assertEquals
 class Day05Test {
     @Test
     fun `part 1 returns a correct result`() {
-        TODO("Implement this")
+        val input = "dabAcCaCBAcCcaDA"
+
+        val correctOutput = "10"
+
+        assertEquals(correctOutput, Day05.solvePart1(input))
     }
 
     @Test
     fun `part 2 returns a correct result`() {
-        TODO("Implement this")
+        val input = "dabAcCaCBAcCcaDA"
+
+        val correctOutput = "4"
+
+        assertEquals(correctOutput, Day05.solvePart2(input))
+    }
+
+    @Test
+    fun `a given polymer gets correctly collapsed`() {
+        val rawInput = "dabAcCaCBAcCcaDA"
+        val input: ArrayList<String> = ArrayList(rawInput.split(""))
+        input.removeIf { it == "" }
+
+        val rawCorrectOutput = "dabCBAcaDA"
+        val correctOutput: ArrayList<String> = ArrayList(rawCorrectOutput.split(""))
+        correctOutput.removeIf { it == "" }
+
+        assertEquals(correctOutput, Day05.collapsePolymer(input))
+    }
+
+    @Test
+    fun `an uncollapsable polymer is gets not further collapsed`() {
+        val rawInput = "dabCBAcaDA"
+        val input: ArrayList<String> = ArrayList(rawInput.split(""))
+        input.removeIf { it == "" }
+
+        assertEquals(input, Day05.collapsePolymer(input))
     }
 }
