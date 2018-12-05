0b54b84019766ced6e1a43382cb361fc8b7893a3
loehnertz <jakob2102@gmail.com>
2018-12-05 20:32:45 +0100

    Begin development of solution for day 5

---
+++ src/main/kotlin/solutions/Day05.kt
@@ -0,0 +1,17 @@
+package solutions
+
+object Day05 : Solution() {
+    override val identifier = this::class.simpleName.toString()
+
+    override fun solvePart1(input: String): String {
+        TODO("Implement this!")
+    }
+
+    override fun solvePart2(input: String): String {
+        TODO("Implement this!")
+    }
+}
+
+fun main(args: Array<String>) {
+    Day05.solve()
+}
---
+++ src/test/kotlin/solutions/Day05Test.kt
@@ -0,0 +1,16 @@
+package solutions
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class Day05Test {
+    @Test
+    fun `part 1 returns a correct result`() {
+        TODO("Implement this")
+    }
+
+    @Test
+    fun `part 2 returns a correct result`() {
+        TODO("Implement this")
+    }
+}
