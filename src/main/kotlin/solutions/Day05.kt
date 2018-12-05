f73e9efcd9b8fc98120a4ef53e413c16c967ece8
loehnertz <jakob2102@gmail.com>
2018-12-05 22:40:09 +0100

    Refactor current code of day 5

--- src/main/kotlin/solutions/Day05.kt
+++ src/main/kotlin/solutions/Day05.kt
@@ -6,30 +6,34 @@ object Day05 : Solution() {
     override val identifier = this::class.simpleName.toString()
 
     override fun solvePart1(input: String): String {
-        val inputList: ArrayList<String> = ArrayList(input.split(""))
+        val initialPolymer: ArrayList<String> = ArrayList(input.split(""))
+        return collapsePolymer(initialPolymer).size.toString()
+    }
+
+    fun collapsePolymer(initialPolymer: ArrayList<String>): ArrayList<String> {
         // Mark the first and last character to start the while-loop as it is an empty character anyway
-        inputList[0] = deleteChar
-        inputList[inputList.size - 1] = deleteChar
+        initialPolymer[0] = deleteChar
+        initialPolymer[initialPolymer.size - 1] = deleteChar
 
-        while (inputList.indexOf(deleteChar) != -1) {
+        while (initialPolymer.indexOf(deleteChar) != -1) {
             // Remove all characters that were marked in the last iteration
-            inputList.removeIf { it == deleteChar }
+            initialPolymer.removeIf { it == deleteChar }
 
-            for (char in 0..(inputList.size - 1)) {
+            for (char in 0..(initialPolymer.size - 1)) {
                 // Remove the current and the next character when they are units of opposite polarity
-                if (char != (inputList.size - 1)) {
-                    val currentUnit: Char = inputList[char].toCharArray().first()
-                    val nextUnit: Char = inputList[char + 1].toCharArray().first()
+                if (char != (initialPolymer.size - 1)) {
+                    val currentUnit: Char = initialPolymer[char].toCharArray().first()
+                    val nextUnit: Char = initialPolymer[char + 1].toCharArray().first()
 
                     if (currentUnit.equals(other = nextUnit, ignoreCase = true) && currentUnit != nextUnit) {
-                        inputList[char] = deleteChar
-                        inputList[char + 1] = deleteChar
+                        initialPolymer[char] = deleteChar
+                        initialPolymer[char + 1] = deleteChar
                     }
                 }
             }
         }
 
-        return inputList.size.toString()
+        return initialPolymer
     }
 
     override fun solvePart2(input: String): String {
