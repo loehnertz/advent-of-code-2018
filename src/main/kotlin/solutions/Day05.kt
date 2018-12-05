b24bb9791318a500f8e72370f0c32ddb870bd2c0
loehnertz <jakob2102@gmail.com>
2018-12-05 22:36:42 +0100

    Implement the solution for part one of day 5

--- src/main/kotlin/solutions/Day05.kt
+++ src/main/kotlin/solutions/Day05.kt
@@ -1,10 +1,35 @@
 package solutions
 
+const val deleteChar = "-"
+
 object Day05 : Solution() {
     override val identifier = this::class.simpleName.toString()
 
     override fun solvePart1(input: String): String {
-        TODO("Implement this!")
+        val inputList: ArrayList<String> = ArrayList(input.split(""))
+        // Mark the first and last character to start the while-loop as it is an empty character anyway
+        inputList[0] = deleteChar
+        inputList[inputList.size - 1] = deleteChar
+
+        while (inputList.indexOf(deleteChar) != -1) {
+            // Remove all characters that were marked in the last iteration
+            inputList.removeIf { it == deleteChar }
+
+            for (char in 0..(inputList.size - 1)) {
+                // Remove the current and the next character when they are units of opposite polarity
+                if (char != (inputList.size - 1)) {
+                    val currentUnit: Char = inputList[char].toCharArray().first()
+                    val nextUnit: Char = inputList[char + 1].toCharArray().first()
+
+                    if (currentUnit.equals(other = nextUnit, ignoreCase = true) && currentUnit != nextUnit) {
+                        inputList[char] = deleteChar
+                        inputList[char + 1] = deleteChar
+                    }
+                }
+            }
+        }
+
+        return inputList.size.toString()
     }
 
     override fun solvePart2(input: String): String {
