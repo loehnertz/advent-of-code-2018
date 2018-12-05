7973f1e1d0011f4c6129978ce70eeb46c0859264
loehnertz <jakob2102@gmail.com>
2018-12-05 02:00:10 +0100

    Implement the function that parses each record entry

--- src/main/kotlin/solutions/Day04.kt
+++ src/main/kotlin/solutions/Day04.kt
@@ -4,6 +4,10 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 const val RecordTimestampPattern = "yyyy-MM-dd HH:mm"
+const val GuardIdRegex = "#[0-9]*"
+const val BeginsShiftRegex = "begins shift"
+const val WakesUpRegex = "wakes up"
+const val FallsAsleepRegex = "falls asleep"
 
 enum class GuardAction {
     BEGINS_SHIFT, FALLS_ASLEEP, WAKES_UP
@@ -19,11 +23,51 @@ object Day04 : Solution() {
     override val identifier = this::class.simpleName.toString()
 
     override fun solvePart1(input: String): String {
-        TODO("Implement this!")
+        val lines: List<String> = splitMultilineInput(input)
+        val recordEntries: ArrayList<Pair<LocalDateTime, String>> = orderRecordEntries(lines)
+        val guardRecords: ArrayList<GuardRecord> = parseRecordEntries(recordEntries)
+
+        return ""
     }
 
     fun calculateGuardWithMostTimeAsleep(guardAsleepTimes: HashMap<Int, Array<Int>>): Pair<Int, Int> {
         TODO("Implement this!")
+    }
+
+    fun parseRecordEntries(recordEntries: ArrayList<Pair<LocalDateTime, String>>): ArrayList<GuardRecord> {
+        val guardRecords: ArrayList<GuardRecord> = ArrayList()
+        var currentAction = GuardAction.BEGINS_SHIFT
+        var currentGuardId = 0
+
+        for (entry: Pair<LocalDateTime, String> in recordEntries) {
+            val stringToParse: String = entry.second
+
+            when {
+                // Guards begins shift
+                stringToParse.contains(Regex(BeginsShiftRegex)) -> {
+                    currentAction = GuardAction.BEGINS_SHIFT
+                    currentGuardId = Regex(GuardIdRegex).find(stringToParse)?.value.toString().replace("#", "").toInt()
+                }
+                // Guards wakes up
+                stringToParse.contains(Regex(WakesUpRegex)) -> {
+                    currentAction = GuardAction.WAKES_UP
+                }
+                // Guards falls asleep
+                stringToParse.contains(Regex(FallsAsleepRegex)) -> {
+                    currentAction = GuardAction.FALLS_ASLEEP
+                }
+            }
+
+            guardRecords.add(
+                GuardRecord(
+                    timestamp = entry.first,
+                    id = currentGuardId,
+                    action = currentAction
+                )
+            )
+        }
+
+        return guardRecords
     }
 
     fun orderRecordEntries(recordLines: List<String>): ArrayList<Pair<LocalDateTime, String>> {
@@ -40,10 +84,6 @@ object Day04 : Solution() {
 
         recordEntries.sortBy { it.first }
         return recordEntries
-    }
-
-    fun parseRecordEntries(recordEntries: ArrayList<Pair<LocalDateTime, String>>): ArrayList<GuardRecord> {
-        TODO("Implement this!")
     }
 
     fun parseTimestamp(timestamp: String): LocalDateTime {
